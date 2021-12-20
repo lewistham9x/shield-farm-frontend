bd1a2eb2ec6ea4351e6863452141f995c8da51c1
lewis <lewistham9x@gmail.com>
2021-12-21 02:06:00 +0800

    Borrowed code from beefy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo, useCallback, useEffect, useMemo, useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import { Helmet } from "react-helmet";
+import { hot, setConfig } from "react-hot-loader";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import {
+  makeStyles,
+  ThemeProvider,
+  StylesProvider,
+} from "@material-ui/core/styles";
+import appStyle from "./jss/appStyle.js";
+import useNightMode from "./hooks/useNightMode";
+import createThemeMode from "./jss/appTheme";
+
+// import { useConnectWallet } from "features/home/redux/hooks";
+import { useFetchApys } from "./hooks/fetchApys";
+import VisiblePools from "./components/VisiblePools";
+
+const FETCH_INTERVAL_MS = 15 * 1000;
+
+const themes = { light: null, dark: null };
+const getTheme = (mode) => {
+  return (themes[mode] = themes[mode] || createThemeMode(mode === "dark"));
+};
 
 function App() {
+  const { pools, fetchApys } = useFetchApys();
+  // const { isNightMode, setNightMode } = useNightMode();
+  const isNightMode = true;
+
+  // console.log("apys", pools);
+  const theme = useMemo(
+    () => getTheme(isNightMode ? "dark" : "light"),
+    [isNightMode]
+  );
+
+  const useStyles = useMemo(() => {
+    return makeStyles(appStyle, { defaultTheme: theme });
+  }, [theme]);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    fetchApys();
+    const id = setInterval(fetchApys, FETCH_INTERVAL_MS);
+    return () => clearInterval(id);
+  }, [fetchApys]);
+
+  // useEffect(() => {
+  //   const fetch = () => {
+  //     if (address && web3 && !fetchBalancesPending) {
+  //       fetchBalances({ address, web3, tokens });
+  //     }
+  //     if (!fetchVaultsDataPending) {
+  //       fetchVaultsData({ web3, pools });
+  //     }
+  //   };
+  //   fetch();
+
+  //   const id = setInterval(fetch, FETCH_INTERVAL_MS);
+  //   return () => clearInterval(id);
+
+  //   // Adding tokens and pools to this dep list, causes an endless loop, DDoSing the api
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [address, web3, fetchBalances, fetchVaultsData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <Grid container className={classes.container}>
+            <VisiblePools pools={pools} />
+          </Grid>
+        </ThemeProvider>
+      </StylesProvider>
+    </HashRouter>
   );
 }
 
-export default App;
+export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./helpers/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <link href="/dist/output.css" rel="stylesheet"></link>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
